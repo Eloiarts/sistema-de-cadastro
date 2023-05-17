3a5fdbed3d77d3f7c5540f1de9a916673aaaddf1
Eloiarts <68865988+Eloiarts@users.noreply.github.com>
2023-05-17 14:43:13 -0300

    Update app.js
    teste

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicando está se conectando ao banco de dados")
 console.log("Mais um conectado")
+console.log("Enviando para o terminal")
 
